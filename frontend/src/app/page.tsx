d6c1a5fbeff63f4854fedbbe8817cd6086682fe7
Samuelle Yimga <samuelle.yimga@reseau.eseo.fr>
2025-10-17 10:58:35 +0200

    feat(front): initialiser la structure du frontend Next.js (app/, components/, context/, hooks/, lib/)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,25 @@
-import Image from "next/image";
-
-
 export default function Home() {
-  return ;
+  return (
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+        <h1 className="text-3xl font-semibold text-blue-700 mb-6">
+          Bienvenue sur MedDataCollect 👨‍⚕️
+        </h1>
+
+        <div className="space-x-4">
+          <a
+              href="/login-medecin"
+              className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg transition"
+          >
+            Connexion Médecin
+          </a>
+
+          <a
+              href="/register-medecin"
+              className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg transition"
+          >
+            Créer un compte
+          </a>
+        </div>
+      </main>
+  );
 }
