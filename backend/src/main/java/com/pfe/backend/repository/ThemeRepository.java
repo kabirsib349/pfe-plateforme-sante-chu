9b234e403574d6aad928c9d7f49cf4a203d05147
kabirsib349 <ibrahim.kabirsaleh@reseau.eseo.fr>
2026-01-08 16:08:11 +0100

    feat: ajout des repositories Theme et QuestionTheme

---
+++ backend/src/main/java/com/pfe/backend/repository/ThemeRepository.java
@@ -0,0 +1,9 @@
+package com.pfe.backend.repository;
+
+import com.pfe.backend.model.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+}
